ea8dc2dd223d81248cccd75cb116108c22df50b4
suman-tech96 <sumanjana9692@gmail.com>
2026-04-17 17:11:06 +0530

    base url changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://ping-me-backend-09ed.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
